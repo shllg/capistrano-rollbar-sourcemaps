51b4c336dfc69d7f7ba1564e3a539808b9353065
Sascha <sascha@wonderweblabs.com>
2017-03-21 04:35:36 +0100

    Update sourcemaps.rb

--- lib/capistrano/rollbar/sourcemaps.rb
+++ lib/capistrano/rollbar/sourcemaps.rb
@@ -3,7 +3,8 @@ load File.expand_path('../../tasks/sourcemaps.rake', __FILE__)
 
 namespace :deploy do
   # after :set_current_revision, 'rollbar:sourcemaps:upload'
-  after 'puma:smart_restart', 'rollbar:sourcemaps:upload'
+  # after 'puma:smart_restart', 'rollbar:sourcemaps:upload'
+  after 'deploy:symlink:release', 'rollbar:sourcemaps:upload'
 end
 
 namespace :load do
